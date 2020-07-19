4abf7dd41c96515e4d35bff5d87351a1931907cd
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-19 16:35:49 +0530

    Functional component creation

--- Academind/Section-3/react-101/src/App.js
+++ Academind/Section-3/react-101/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <h1 className="App-title">Welcome to React</h1>
+          <Person />
       </div>
     );
   }
