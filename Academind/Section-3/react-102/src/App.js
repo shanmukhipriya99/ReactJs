b568ea19f1efa61728da92874dfc80509b35fce4
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-19 23:20:09 +0530

    Inline styles

--- Academind/Section-3/react-102/src/App.js
+++ Academind/Section-3/react-102/src/App.js
@@ -35,11 +35,19 @@ class App extends Component {
   }
 
   render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '2px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>        
-        <button onClick={() => this.switchNameHandler('Shanmukhi!!')}>Switch Name</button>      
+        <button style={style}
+        onClick={() => this.switchNameHandler('Shanmukhi!!')}>Switch Name</button>      
         <Person                 // 1st way to pass a paramter above
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} />
