8f8cd7d3a8659645e2b472443248553cfd8f961e
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-19 17:28:14 +0530

    Working with 'props'

--- Academind/Section-3/react-101/src/Person/Person.js
+++ Academind/Section-3/react-101/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';  
 
-const person = () => {
-    return <p>I'm a person! <br /> Random Number: {Math.floor(Math.random() * 30)} </p>
+const person = (props) => {
+return <p>I'm {props.name} and I'm {props.age} years old! <br /> Random Number: {Math.floor(Math.random() * 30)} </p>
 }
 
 export default person;
