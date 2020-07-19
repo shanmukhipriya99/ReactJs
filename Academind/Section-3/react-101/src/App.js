12b5c57c8b102c433ca36bc1d8af5e31dec4ac63
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-19 19:24:54 +0530

    Manipulating the state

--- Academind/Section-3/react-101/src/App.js
+++ Academind/Section-3/react-101/src/App.js
@@ -9,12 +9,22 @@ class App extends Component {
       { name: 'Stephan', age: 22}
     ]
   }
+switchNameHandler = () => {
+  // console.log("Was clicked!");
+  // this.state.persons[0].name = 'Shanmukhi Priya';  Wrong approach
+  this.setState({    // setState() is a function from Component
+    persons: [
+      { name: 'Shanmukhi Priya', age: 21},
+      { name: 'Stephan', age: 21}
+    ]
+  });
+}
 
   render() {
     return (
       <div className="App">
           <h1>Welcome to React</h1>
-          <button>Switch Name!</button>
+          <button onClick={this.switchNameHandler}>Switch Name!</button>
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> Random number generated!</Person>
           <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> Random number generated!</Person>
       </div>
