47bc1e168b3f017b548e8ecb13e8c5cbc8a8f3fc
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-19 17:21:22 +0530

    Outputting simple dynamic content

--- Academind/Section-3/react-101/src/Person/Person.js
+++ Academind/Section-3/react-101/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';  
 
 const person = () => {
-    return <p>I'm a person!</p>
+    return <p>I'm a person! <br /> Random Number: {Math.floor(Math.random() * 30)} </p>
 }
 
 export default person;
