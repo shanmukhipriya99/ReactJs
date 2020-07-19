e68717a6c1f9f95cab0a684a3988efb998b6b498
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-19 18:16:38 +0530

    Working with state

--- Academind/Section-3/react-101/src/App.js
+++ Academind/Section-3/react-101/src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {        // can be used only if class extends to component
+    persons: [
+      { name: 'Shanmukhi', age: 21},
+      { name: 'Stephan', age: 22}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-          <h1 className="App-title">Welcome to React</h1>
-          <Person name='Shanmukhi' age='21'> Random number generated!</Person>
+          <h1>Welcome to React</h1>
+          <button>Switch Name!</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> Random number generated!</Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> Random number generated!</Person>
       </div>
     );
   }
