2496ea7b78144817f3c6e41e996e695f0f7b015e
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-20 22:39:07 +0530

    removed all style packages

--- Academind/Section-5/react-102/src/App.js
+++ Academind/Section-5/react-102/src/App.js
@@ -1,20 +1,6 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green' };
-  color: white;
-  font: inherit;
-  border: 2px solid blue;
-  padding: 8px;
-  cursor: pointer;
-  &:hover {   
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen' };
-    color: black;
-  }
-`;      //note the &
 
 class App extends Component {
   state = {
@@ -52,14 +38,14 @@ class App extends Component {
   }
 
   render () {
-    // const style = {
-    //   backgroundColor: 'green',
-    //   color: 'white',
-    //   font: 'inherit',
-    //   border: '2px solid blue',
-    //   padding: '8px',
-    //   cursor: 'pointer'
-    // };
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '2px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
 
     let persons = null;
     if ( this.state.showPersons ) {
@@ -75,7 +61,7 @@ class App extends Component {
         })}
         </div>
       );
-      // style.backgroundColor = 'red';
+      style.backgroundColor = 'red';
     }
 
     const classes = [];
@@ -90,9 +76,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>        
-        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonHandler}>
+        <button style={style} onClick={this.togglePersonHandler}>
           Toggle Persons  
-        </StyledButton>     
+        </button>     
         {persons}
       </div>
     );
--- Academind/Section-5/react-102/src/Person/Person.js
+++ Academind/Section-5/react-102/src/Person/Person.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
-// import './Person.css';
-
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-        
-    @media (min-width: 500px) {
-    width: 450px;
-    }
-`;
+import './Person.css';
 
 const person = ( props ) => {
     return (
-        // <div className='Person' onClick={props.click}>
-        <StyledDiv>
+        <div className='Person' onClick={props.click}>
             <p>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </StyledDiv>      
+        </div>
     )
 };
 
