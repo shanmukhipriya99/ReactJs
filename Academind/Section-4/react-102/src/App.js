ffd58037e3eec75ffa62ed8cc74807ff640bc68e
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-20 12:15:54 +0530

    outputting lists

--- Academind/Section-4/react-102/src/App.js
+++ Academind/Section-4/react-102/src/App.js
@@ -53,17 +53,9 @@ class App extends Component {
     if ( this.state.showPersons ) {
       persons = (
       <div>
-        <Person                 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} />
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Shan!')}   // .bind is the 2nd way to pass a parameter (more preferable)
-          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} />
+        {this.state.persons.map(person => {  // map() maps all the elements of the array to the JSX code
+          return <Person name={person.name} age={person.age}/>
+        })}
         </div>
       );
     }
