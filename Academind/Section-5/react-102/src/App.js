ea480503078e60f5c54754cff8243ebeb39ba88b
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-20 18:26:15 +0530

    introduction to styled-components

--- Academind/Section-5/react-102/src/App.js
+++ Academind/Section-5/react-102/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
-
 
 class App extends Component {
   state = {
@@ -46,11 +44,7 @@ class App extends Component {
       font: 'inherit',
       border: '2px solid blue',
       padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
+      cursor: 'pointer'
     };
 
     let persons = null;
@@ -68,10 +62,6 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
-      style[':hover'] = {     //since radium and ':hover' is a string, it needs to be passed in []
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
     }
 
     const classes = [];
@@ -83,19 +73,16 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
+      <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>        
         <button style={style}
          onClick={this.togglePersonHandler}>Toggle Persons</button>     
         {persons}
       </div>
-      </StyleRoot>
-      
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App);
+export default App;
