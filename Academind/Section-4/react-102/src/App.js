0a32ba2ee5523432410e2679e72678325c7c1fa8
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-20 12:02:13 +0530

    dynamic content the "Js" way

--- Academind/Section-4/react-102/src/App.js
+++ Academind/Section-4/react-102/src/App.js
@@ -48,29 +48,33 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
+
+    let persons = null;
+    if ( this.state.showPersons ) {
+      persons = (
+      <div>
+        <Person                 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Shan!')}   // .bind is the 2nd way to pass a parameter (more preferable)
+          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>        
         <button style={style}
-        onClick={this.togglePersonHandler}>Toggle Persons</button> 
-        {              
-          this.state.showPersons ?
-            <div>
-            <Person                 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age} />
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Shan!')}   // .bind is the 2nd way to pass a parameter (more preferable)
-              changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age} />
-            </div>  : null
-        }
-        
+         onClick={this.togglePersonHandler}>Toggle Persons</button>     
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
