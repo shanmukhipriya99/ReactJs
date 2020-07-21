7911ba62eed111a9f50f2502440773941519932d
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-21 09:13:12 +0530

    using this.props

--- Academind/Section-7/react-102/src/components/Cockpit/Cockpit.js
+++ Academind/Section-7/react-102/src/components/Cockpit/Cockpit.js
@@ -23,7 +23,7 @@ const cockpit = (props) => {
 
     return (
         <div>
-            <h1>Hi, I'm a React App</h1>
+            <h1>{props.title}</h1>
             <p className={classes.join(' ')}>This is really working!</p>        
             <button style={style} onClick={props.clicked}>
             Toggle Persons  
--- Academind/Section-7/react-102/src/containers/App.js
+++ Academind/Section-7/react-102/src/containers/App.js
@@ -52,7 +52,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Cockpit showPersons={this.state.showPersons} 
+        <Cockpit 
+          title={this.props.appTitle}
+          showPersons={this.state.showPersons} 
           persons={this.state.persons}
           clicked={this.togglePersonHandler}/>
         {persons}
