d20cea6c9b678320a4f4c621b8366ee9efd85e8e
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-22 15:55:16 +0530

    Displaying the build controls

--- Academind/burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ Academind/burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import BuildControl from '../'
+import classes from './BuildControls.module.css';
+import BuildControl from '../BuildControls/BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Cheese', type: 'cheese'},
+    { label: 'Meat', type: 'meat'}
+];
 
 const buildControls = (props) => (
-    <div>
-        
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label}/>
+        ))}
     </div>
 );
 
