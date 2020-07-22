30c2536a5ea52797e1fd4efb720adbfd87f21f48
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-22 19:43:47 +0530

    Added 'Current Price'

--- Academind/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Academind/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -61,7 +61,8 @@ class BurgerBuilder extends Component {
                 <BuildControls 
                 ingredientAdded={this.addIngredientHandler}
                 ingredientRemoved={this.removeIngredientHandler}
-                disabled={disabledInfo}/>
+                disabled={disabledInfo}
+                price={this.state.totalPrice}/>
             </Aux>
         );
     }
