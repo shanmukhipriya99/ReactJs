4989b38bc198050436f6fe93ce58472e2ce24ef3
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-22 12:02:44 +0530

    Added Layout.module.css

--- Academind/burger-builder/src/components/Layout/Layout.js
+++ Academind/burger-builder/src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
         <div>
         toolbar, sidedrawer, backdrop
         </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
