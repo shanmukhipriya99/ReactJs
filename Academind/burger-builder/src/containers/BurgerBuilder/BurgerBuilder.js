3c24ccee26c301b838965490b74a56bd7bef50ce
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-23 11:10:57 +0530

    Showing & hiding the modal

--- Academind/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Academind/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,6 +22,7 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 4,
     purchaseable: false,
+    purchasing: false
   };
 
   updatePurchaseState(ingredients) {
@@ -64,6 +65,10 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients);
   };
 
+  purchaseHandler = () => {
+    this.setState({purchasing: true});
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients,
@@ -73,7 +78,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -82,6 +87,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           purchaseable={this.state.purchaseable}
+          ordered={this.purchaseHandler}
           price={this.state.totalPrice}
         />
       </Aux>
