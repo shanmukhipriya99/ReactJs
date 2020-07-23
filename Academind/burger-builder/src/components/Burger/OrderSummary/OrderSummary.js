a0d37d69453e0f6bab1684b65ca3a3dc569032d3
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-23 10:56:51 +0530

    Order summary added

---
+++ Academind/burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Aux from '../../../hoc/Auxiliary';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igkey => {
+            return <li key={igkey}><span style={{textTransform: 'capitalize'}}>{igkey}:</span> {props.ingredients[igkey]}</li>
+        });
+    return (
+        <Aux>
+            <h3>Your order:</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to checkout??</p>
+        </Aux>
+    );
+};
+
+export default orderSummary;
