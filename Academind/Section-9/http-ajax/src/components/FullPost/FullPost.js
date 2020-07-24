483453ace943d26a3afcd7cfaf52643a4dbacdcb
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-24 16:00:31 +0530

    Deleting a post

--- Academind/Section-9/http-ajax/src/components/FullPost/FullPost.js
+++ Academind/Section-9/http-ajax/src/components/FullPost/FullPost.js
@@ -9,7 +9,10 @@ class FullPost extends Component {
   };
   componentDidUpdate() {
     if (this.props.id) {
-      if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+      ) {
         axios
           .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
           .then((response) => {
@@ -18,6 +21,15 @@ class FullPost extends Component {
       }
     }
   }
+
+  deletePostHandler = () => {
+    axios
+      .delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
     if (this.props.id) {
@@ -29,7 +41,9 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.content}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button onClick={this.deletePostHandler} className="Delete">
+              Delete
+            </button>
           </div>
         </div>
       );
