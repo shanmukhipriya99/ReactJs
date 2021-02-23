bfdb9ce7f08885adf66c809e9d732e32e170ea36
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2021-02-23 15:14:03 +0530

    working with arras immutably

--- Academind/Section-14/redux--01-start/src/containers/Counter/Counter.js
+++ Academind/Section-14/redux--01-start/src/containers/Counter/Counter.js
@@ -19,14 +19,23 @@ class Counter extends Component {
         />
         <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
         <CounterControl label="Subtract 5" clicked={this.props.onSubCounter} />
+        <hr />
+        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <ul>
+            {this.props.storedResults.map(result => {
+                <li key={result.id} onClick={() => this.props.onDeleteResult(result.id)}>{result.value}</li>
+            })};
+        </ul>
       </div>
+      
     );
   }
 }
-//this has to be after class ans before export
+//this has to be after class and before export
 const mapStateToProps = (state) => {
   return {
     ctr: state.counter,
+    storedResults: state.result
   };
 };
 
@@ -36,6 +45,8 @@ const mapDispatchToProps = (dispatch) => {
     onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
     onAddCounter: () => dispatch({ type: "ADD", value: 5 }),
     onSubCounter: () => dispatch({ type: "SUB", value: 5 }),
+    onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
+    onDeleteResult: (id) => dispatch({ type: "DELETE_RESULT", resultElId: id })
   };
 };
 
--- Academind/Section-14/redux--01-start/src/store/reducer.js
+++ Academind/Section-14/redux--01-start/src/store/reducer.js
@@ -1,5 +1,6 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    result: []
 }
 
 const reducer = (state = initialState, action) => {
@@ -26,6 +27,22 @@ const reducer = (state = initialState, action) => {
             ...state,
             counter: state.counter - action.value
         }
+    }
+    if(action.type === 'STORE_RESULT'){
+        return {
+            ...state,
+            result: state.result.concat({id: new Date(),value: state.counter})  //push() also does the same thing but mutates the original array
+        }         // whereas concat() returns a new array (old array + changes made)
+    }
+    if(action.type === 'DELETE_RESULT'){
+        // const id = 2;     //if it's an array of objects, then the objects also need to be changed immutably
+        // const newArray = [...state.result];  //for immutable changes
+        // newArray.splice(id, 1);
+        const updatedArray = state.result.filter(result => result.id !== action.resultElId);
+        return {
+            ...state,
+            result: updatedArray
+        }         
     }
     return state;
 }
