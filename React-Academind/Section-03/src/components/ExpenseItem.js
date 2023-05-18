bcbb174395d9d408e019aa0c7961c832f1f6bc6e
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-17 18:20:40 -0700

    ExpenseDate component

--- React-Academind/Section-03/src/components/ExpenseItem.js
+++ React-Academind/Section-03/src/components/ExpenseItem.js
@@ -1,19 +1,13 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-//   const expenseDate = new Date(2021, 3, 29);
-//   const expenseTitle = 'Insurance';
-//   const expenseAmount = 300;
-const month = props.date.toLocaleString('en-US', {month: 'long'});
-const year = props.date.getFullYear();
-const day = props.date.toLocaleString('en-US', {day: '2-digit'})
+  //   const expenseDate = new Date(2021, 3, 29);
+  //   const expenseTitle = 'Insurance';
+  //   const expenseAmount = 300;
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
