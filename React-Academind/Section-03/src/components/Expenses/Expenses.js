3882608da22b99a3aec6559af85244bb31c6dbab
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-17 22:42:48 -0700

    organizing content

--- React-Academind/Section-03/src/components/Expenses/Expenses.js
+++ React-Academind/Section-03/src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
-import Card from './Card';
+import Card from '../UI/Card';
 
 // function Expenses(props) {
 //   return (
@@ -14,15 +14,17 @@ import Card from './Card';
 //   );
 // }
 function Expenses(props) {
-    return (
-      <Card className='expenses'>
-      {props.expenses.map(expense => <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      ></ExpenseItem>)}
-      </Card>
-    );
-  }
+  return (
+    <Card className='expenses'>
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        ></ExpenseItem>
+      ))}
+    </Card>
+  );
+}
 
 export default Expenses;
