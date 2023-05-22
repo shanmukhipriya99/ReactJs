fe8a3547d3d7e0b99335e4a776b5fc0bd0bd46fb
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-22 13:54:21 -0700

    useState

--- React-Academind/Section-03/src/components/Expenses/ExpenseItem.js
+++ React-Academind/Section-03/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -7,14 +7,17 @@ function ExpenseItem(props) {
   //   const expenseDate = new Date(2021, 3, 29);
   //   const expenseTitle = 'Insurance';
   //   const expenseAmount = 300;
+
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log('Clicked');
+    setTitle('Updated!');
   };
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
