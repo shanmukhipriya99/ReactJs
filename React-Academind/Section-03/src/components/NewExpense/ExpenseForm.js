220f798aa8154ce4c6a3aa9672e2bd7ac3433789
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-22 15:56:02 -0700

    form submission and two-way binding

--- React-Academind/Section-03/src/components/NewExpense/ExpenseForm.js
+++ React-Academind/Section-03/src/components/NewExpense/ExpenseForm.js
@@ -19,20 +19,44 @@ const ExpenseForm = () => {
     event.preventDefault();
     setDate(event.target.value);
   };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title,
+      amount,
+      date: new Date(date),
+    };
+    console.log(expenseData);
+    setTitle('');
+    setAmount('');
+    setDate('');
+  };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input type='text' value={title} onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            value={amount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2023-12-31' onChange={dateChangeHandler} />
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2023-12-31'
+            value={date}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className='new-expense__actions'>
