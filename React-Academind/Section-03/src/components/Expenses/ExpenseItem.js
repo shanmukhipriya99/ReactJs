98a5c6ffc44eb7aeeca091987af75f847585e62a
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-22 13:38:30 -0700

    event handlers

--- React-Academind/Section-03/src/components/Expenses/ExpenseItem.js
+++ React-Academind/Section-03/src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -6,6 +7,9 @@ function ExpenseItem(props) {
   //   const expenseDate = new Date(2021, 3, 29);
   //   const expenseTitle = 'Insurance';
   //   const expenseAmount = 300;
+  const clickHandler = () => {
+    console.log('Clicked');
+  };
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}></ExpenseDate>
@@ -13,6 +17,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
