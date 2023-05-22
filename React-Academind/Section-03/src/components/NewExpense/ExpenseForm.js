ccb4cc74c3f2532ce9f11809534637fd109d5390
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-22 15:21:19 -0700

    multiple states

--- React-Academind/Section-03/src/components/NewExpense/ExpenseForm.js
+++ React-Academind/Section-03/src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+
+  const titleChangeHandler = (event) => {
+    event.preventDefault();
+    setTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    event.preventDefault();
+    setAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    event.preventDefault();
+    setDate(event.target.value);
+  };
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' />
+          <input type='text' onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2023-12-31' />
+          <input type='date' min='2019-01-01' max='2023-12-31' onChange={dateChangeHandler} />
         </div>
       </div>
       <div className='new-expense__actions'>
