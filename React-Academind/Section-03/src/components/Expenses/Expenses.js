26ed50811095cea8eaf6c18a20da3fb3161a590d
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-28 10:57:58 -0700

    assignment 2

--- React-Academind/Section-03/src/components/Expenses/Expenses.js
+++ React-Academind/Section-03/src/components/Expenses/Expenses.js
@@ -1,21 +1,19 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
+import { useState } from 'react';
 
-// function Expenses(props) {
-//   return (
-//     <Card className='expenses'>
-//       <ExpenseItem
-//         title={props.expense.title}
-//         amount={props.expense.amount}
-//         date={props.expense.date}
-//       ></ExpenseItem>
-//     </Card>
-//   );
-// }
 function Expenses(props) {
+  const [year, setYear] = useState('2022');
+
+  const selectedYearHandler = (year) => {
+    setYear(year);
+  };
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter selected={year} onYearSelected={selectedYearHandler} />
       {props.expenses.map((expense) => (
         <ExpenseItem
           title={expense.title}
@@ -28,3 +26,15 @@ function Expenses(props) {
 }
 
 export default Expenses;
+
+// function Expenses(props) {
+//   return (
+//     <Card className='expenses'>
+//       <ExpenseItem
+//         title={props.expense.title}
+//         amount={props.expense.amount}
+//         date={props.expense.date}
+//       ></ExpenseItem>
+//     </Card>
+//   );
+// }
