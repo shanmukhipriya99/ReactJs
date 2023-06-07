dca711235e648382978547d7e1ebe5c1c8336de8
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-06-06 18:30:10 -0700

    conditional return statements

--- React-Academind/Section-03/src/components/Expenses/ExpenseItem.js
+++ React-Academind/Section-03/src/components/Expenses/ExpenseItem.js
@@ -14,14 +14,16 @@ function ExpenseItem(props) {
     setTitle('Updated!');
   };
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date}></ExpenseDate>
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date}></ExpenseDate>
+        <div className='expense-item__description'>
+          <h2>{title}</h2>
+          <div className='expense-item__price'>{props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Change Title</button>
+      </Card>
+    </li>
   );
 }
 
--- React-Academind/Section-03/src/components/Expenses/Expenses.js
+++ React-Academind/Section-03/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import { useState } from 'react';
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
   const [year, setYear] = useState('2022');
@@ -15,22 +15,11 @@ function Expenses(props) {
     (expense) => expense.date.getFullYear() === parseInt(year)
   );
 
-  let expensesContent = <p>No expenses found!</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      ></ExpenseItem>
-    ));
-  }
 
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={year} onYearSelected={selectedYearHandler} />
-      {expensesContent}
+      <ExpensesList expenses={filteredExpenses} />
     </Card>
   );
 }
