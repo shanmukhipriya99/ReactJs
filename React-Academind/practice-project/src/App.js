8e1b68f1da940c3c73f8072a7ac7e8398e4a093c
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-06-07 17:03:34 -0700

    user form complete

--- React-Academind/practice-project/src/App.js
+++ React-Academind/practice-project/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import UserForm from './components/UserForm';
 
 function App() {
   return (
     <div>
-
+      <UserForm />
     </div>
   );
 }
---
+++ React-Academind/practice-project/src/components/UserForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './UserForm.css';
+
+const UserForm = () => {
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState('');
+
+  const usernameHandler = (event) => {
+    setUsername(event.target.value.trim());
+  };
+
+  const ageHandler = (event) => {
+    setAge(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    const user = {
+      username,
+      age,
+    };
+    console.log(user);
+    setUsername('');
+    setAge('');
+  };
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className='user-form'>
+        <label>Username:</label>
+        <input type='text' value={username} onChange={usernameHandler} />
+        <label>Age (Years):</label>
+        <input type='number' value={age} onChange={ageHandler} />
+        <button type='submit'>Add User</button>
+      </div>
+    </form>
+  );
+};
+
+export default UserForm;
