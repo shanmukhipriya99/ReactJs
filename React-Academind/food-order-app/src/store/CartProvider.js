5aedcf0b52f13dafc8518b2fb6fbd8b9d4b84229
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-06-13 18:18:40 -0700

    Added Cart reducer

--- React-Academind/food-order-app/src/store/CartProvider.js
+++ React-Academind/food-order-app/src/store/CartProvider.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if(action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item); //concat returns a brand new array
+    const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+    return {items: updatedItems, totalAmount: updatedTotalAmount}
+  } else if(action.type === 'REMOVE') {
+    // return {items: , totalAmount:}
+  }
+  return defaultCartState;
+};
+
 const CartProvider = (props) => {
-  const itemToCart = (item) => {};
-  const itemFromCart = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const itemToCart = (item) => {
+    dispatchCartAction({ type: 'ADD', item: item });
+  };
+  const itemFromCart = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: itemToCart,
     removeItem: itemFromCart,
   };
