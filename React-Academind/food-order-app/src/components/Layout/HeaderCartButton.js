60a2b9d2342397b95dba90991d262bc3290528b2
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-06-15 16:11:51 -0700

    useEffect

--- React-Academind/food-order-app/src/components/Layout/HeaderCartButton.js
+++ React-Academind/food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const [buttonHighlighted, setButtonHighlighted] = useState(false);
   const ctx = useContext(CartContext);
   const numOfCartItems = ctx.items.reduce((currentNum, item) => {
     return currentNum + item.amount;
   }, 0);
+
+  const { items } = ctx;
+
+  const bttnClasses = `${classes.button} ${
+    buttonHighlighted ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (ctx.items.length === 0) {
+      return;
+    }
+    setButtonHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [ctx.items.length, items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={bttnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
