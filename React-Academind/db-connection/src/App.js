3b280c51cc4f01af76b25ba9067b4beaa96e8e8f
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-06-20 11:33:58 -0700

    handling loading and data states

--- React-Academind/db-connection/src/App.js
+++ React-Academind/db-connection/src/App.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  // const dummyMovies = [
+  //   {
+  //     id: 1,
+  //     title: 'Some Dummy Movie',
+  //     openingText: 'This is the opening text of the movie',
+  //     releaseDate: '2021-05-18',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Some Dummy Movie 2',
+  //     openingText: 'This is the second opening text of the movie',
+  //     releaseDate: '2021-05-19',
+  //   },
+  // ];
+
+  async function fetchMoviesHandler() {
+    setLoading(true);
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+    setMovies(data.results);
+    setLoading(false);
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {!loading && movies.length > 0 && <MoviesList movies={movies} /> }
+        {!loading && movies.length === 0 && <p>Found no movies.</p>}
+        {loading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
