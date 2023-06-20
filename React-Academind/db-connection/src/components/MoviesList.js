89f1ca6c4c677a54a0c26e7209af0df52962d58f
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-06-20 11:33:45 -0700

    GET request

--- React-Academind/db-connection/src/components/MoviesList.js
+++ React-Academind/db-connection/src/components/MoviesList.js
@@ -8,10 +8,10 @@ const MovieList = (props) => {
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
         <Movie
-          key={movie.id}
+          key={movie.episode_id}
           title={movie.title}
-          releaseDate={movie.releaseDate}
-          openingText={movie.openingText}
+          releaseDate={movie.release_date}
+          openingText={movie.opening_crawl}
         />
       ))}
     </ul>
