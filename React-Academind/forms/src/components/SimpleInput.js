c7764914cc82ab90f1e19e3ea8e14d8ff756af8e
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-06-22 10:55:58 -0700

    basic form validation

--- React-Academind/forms/src/components/SimpleInput.js
+++ React-Academind/forms/src/components/SimpleInput.js
@@ -1,11 +1,33 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const nameRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if(enteredName.trim() == '') {
+      return;
+    }
+    console.log(enteredName);
+    console.log(nameRef.current.value);
+    setEnteredName('');
+  };
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={nameRef}
+          type='text'
+          id='name'
+          value={enteredName}
+          onChange={nameChangeHandler}
+        />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
